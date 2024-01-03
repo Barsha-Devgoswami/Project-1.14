73e618f1fccf362d8e9117b7b9d75b932c708163
Barsha-Devgoswami <devgoswamibarsha18@gmail.com>
2024-01-03 22:10:11 +0530

    initial commit

---
+++ Retrieve_data/Retrieve_data/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retrieve_data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr;
+            try
+            {
+                string fpath = "D:\\Project 1.14\\student_data.txt";
+                sr = new StreamReader(fpath);
+                string text = " ";
+                while ((text = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(text);
+                }
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error!!!" + ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
